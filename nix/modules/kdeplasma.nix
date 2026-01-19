96c8bec930013c0ec49f7884c498761e24383344
ckhrix <jannickirvold@gmail.com>
2026-01-19 01:33:50 +0100

    added basic packages

--- nix/modules/kdeplasma.nix
+++ nix/modules/kdeplasma.nix
@@ -8,6 +8,14 @@
         variant = "";
     };
 
+    environment.systemPackages = with pkgs; [
+        kdePackages.kdenlive
+        kdePackages.filelight
+        kdePackages.kate
+        kdePackages.konsole
+        kdePackages.dolphin
+    ];
+
     environment.plasma6.excludePackages = with pkgs.kdePackages; [
         plasma-browser-integration # The browser extension bridge
         oxygen                     # Old icon/cursor theme
--- nix/packages.nix
+++ nix/packages.nix
@@ -2,13 +2,41 @@
   nixpkgs.config.allowUnfree = true;
 
   networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
   services.printing.enable = true;
   programs.firefox.enable = true;
 
   environment.systemPackages = with pkgs; [
-    vim
+    # System
+    neovim
     wget
     git
     fastfetch
+    coolercontrol.coolercontrold
+    coolercontrol.coolercontrol-gui
+    lact
+    openrgb
+    qpwgraph
+
+    # Gaming
+    osu-lazer-bin
+    steam
+    vesktop
+    ungoogled-chromium
+    lutris
+    protontricks
+
+    # Creative
+    cameractrls
+    audacity
+    celluloid
+    gimp
+    krita
+    obs-studio
+    rnote
+    speedcrunch
+
+    # KDE Packages are under kdeplasma.nix
+    # Sway Packages are under sway.nix
   ];
 }
