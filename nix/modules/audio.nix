9b0dace596e844fa495b05097959015c298cd005
ckhrix <jannickirvold@gmail.com>
2026-01-22 17:50:21 +0100

    changed audio

--- nix/modules/audio.nix
+++ nix/modules/audio.nix
@@ -6,7 +6,7 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
-        jack.enable = true;
+        jack.enable = false;
 
         extraConfig.pipewire = {
           "99-low-latency" = {
