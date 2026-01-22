487a9e8a84e3cd5b115321bb5cc0a2135ada4824
ckhrix <jannickirvold@gmail.com>
2026-01-22 19:46:57 +0100

    edited locale

--- nix/modules/locale.nix
+++ nix/modules/locale.nix
@@ -12,7 +12,7 @@
     LC_NUMERIC = "da_DK.UTF-8";
     LC_PAPER = "da_DK.UTF-8";
     LC_TELEPHONE = "da_DK.UTF-8";
-    LC_TIME = "da_DK.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
   };
 
   console.keyMap = "dk-latin1";
